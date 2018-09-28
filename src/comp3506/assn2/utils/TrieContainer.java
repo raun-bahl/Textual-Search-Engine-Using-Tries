17bd1468661a6af28c6b4a6937187f673b794ab6
Raun Bahl <r.bahl@uqconnect.edu.au>
2018-09-29 09:19:54 +1000

    "Added TrieContainer constructor"

--- src/comp3506/assn2/utils/TrieContainer.java
+++ src/comp3506/assn2/utils/TrieContainer.java
@@ -4,4 +4,12 @@ public class TrieContainer {
 
     public boolean isEnd;
     public TrieContainer[] series = new TrieContainer[26];
+
+    public TrieContainer() {
+        isEnd = false;
+
+        for (int i =0; i< 26; i++) {
+            series[i] = null;
+        }
+    }
 }
